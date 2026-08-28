ea27148fb183fb723e4968b790c70357c764a88e
Jeffrie <j@j95.us>
2026-08-27 23:57:51 -0400

    Updates.

--- objects/skaters/stats.gd
+++ objects/skaters/stats.gd
@@ -16,11 +16,13 @@ func _init() -> void:
 	for X in ClassTypes.values():
 		Classes.push_back(StatBlock.new())
 	Classes[ClassTypes.LIGHT].weight = 1
-	Classes[ClassTypes.LIGHT].speed = 7
+	Classes[ClassTypes.LIGHT].speed = 5
 	Classes[ClassTypes.LIGHT].sprite_index = 60
+	
 	Classes[ClassTypes.MEDIUM].weight = 1.5
-	Classes[ClassTypes.MEDIUM].speed = 5
+	Classes[ClassTypes.MEDIUM].speed = 5.5
 	Classes[ClassTypes.MEDIUM].sprite_index = 0
+	
 	Classes[ClassTypes.HEAVY].weight = 2
-	Classes[ClassTypes.HEAVY].speed = 3
+	Classes[ClassTypes.HEAVY].speed = 5.5
 	Classes[ClassTypes.HEAVY].sprite_index = 32
